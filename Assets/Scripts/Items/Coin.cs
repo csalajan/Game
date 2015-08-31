9ec1ca489e1cb7d9e1e1db75a0772482c015d266
Tyler <tyjfox@umich.edu>
2015-08-31 00:58:14 -0400

    Fixed coins, added fruit

---
+++ Assets/Scripts/Items/Coin.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+	Vector3 offset;
+	Vector3 pos1;
+	Vector3 pos2; 
+	float moveSpeed = 0.8F;
+	Vector3 moveTo;
+
+	void Start () {
+		offset = Vector3.down;
+		pos1 = transform.position;
+		pos2 = transform.position + offset; 
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		Destroy(this.gameObject);
+	}
+
+	void Update () {
+		if(transform.position == pos1)
+		{
+			moveTo = pos2;
+		}
+		if(transform.position == pos2)
+		{
+			moveTo = pos1;
+		}
+		
+		transform.position = Vector3.MoveTowards(transform.position, moveTo, moveSpeed*Time.deltaTime);
+		transform.Rotate(new Vector3(0, 0, 25*Time.deltaTime));
+	}
+}
---
+++ Assets/Scripts/Items/Fruit.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fruit : MonoBehaviour {
+
+	Vector3 offset;
+	Vector3 pos1;
+	Vector3 pos2; 
+	float moveSpeed = 0.8F;
+	Vector3 moveTo;
+
+	float delay = 5.0F;
+	bool collected = false;
+	
+	void Start () {
+		offset = Vector3.down;
+		pos1 = transform.position;
+		pos2 = transform.position + offset; 
+	}
+	
+	void OnTriggerEnter (Collider col)
+	{
+		transform.Translate (1000, 1000, 1000);
+		collected = true;
+	}
+	
+	void Update () {
+		if (collected)
+			delay -= Time.deltaTime;
+
+		if (delay <= 0.0F)
+			Application.LoadLevel (0);
+
+		if(transform.position == pos1)
+		{
+			moveTo = pos2;
+		}
+		if(transform.position == pos2)
+		{
+			moveTo = pos1;
+		}
+		
+		transform.position = Vector3.MoveTowards(transform.position, moveTo, moveSpeed*Time.deltaTime);
+		transform.Rotate(new Vector3(0, 25*Time.deltaTime, 0));
+	}
+}
