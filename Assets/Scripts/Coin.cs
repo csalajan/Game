11199dc9b0de19a58849922f40c39e242781f261
Tyler <tyjfox@umich.edu>
2015-08-31 00:28:07 -0400

    Made some prefabs and got frustrated with some coins

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+	void OnTriggernEnter (Collision col)
+	{
+		Destroy(this.gameObject);
+	}
+
+	void Update () {
+		transform.Rotate(new Vector3(0, 0, 25*Time.deltaTime));
+	}
+}
